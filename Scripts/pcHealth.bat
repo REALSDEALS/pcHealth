81d68e85fa87c6c3f91814d3bf0d2b935a5be55f
REALSDEALS <realsdeals@gmail.com>
2021-11-29 23:50:27 +0100

    Update: Main Code
    Update: Updated/Added/Tweaked the code.
    
    A new function has been added; to let you scan and repair right after eachother.
    
    Tweaked how the code worked, it should be more streamlined.
    
    Updated the code as stated above and updated the changelog.md file.

--- Scripts/pcHealth.bat
+++ Scripts/pcHealth.bat
@@ -1,6 +1,6 @@
 echo off
 cd /
-color 6
+color B
 cls
 echo Thanks for downloading and using pcHealth!
 echo Please be sure that you are running this Batch file in Administrator mode.
@@ -10,24 +10,36 @@ echo.
 echo ........................................
 echo PRESS 1 TO RUN A SYSTEM SCAN
 echo PRESS 2 TO TRY AND REPAIR CORRUPT FILES
-echo PRESS 3 TO GET A BATTERY REPORT
-echo PRESS 4 TO CLOSE THIS BATCH FILE
+echo PRESS 3 TO RUN A SYSTEM SCAN AND START REPAIRING IN ONE GO
+echo PRESS 4 TO GET A BATTERY REPORT
+echo PRESS 5 TO CLOSE THIS BATCH FILE
 echo ........................................
 echo.
 SET /P M=Type 1, 2, 3, or 4 then press ENTER:
 IF %M%==1 GOTO SCAN
 IF %M%==2 GOTO DISM
-IF %M%==3 GOTO BATTERY
-IF %M%==4 GOTO CLOSE
+IF %M%==3 GOTO SCSM
+IF %M%==4 GOTO BATTERY
+IF %M%==5 GOTO CLOSE
 :SCAN
 sfc /scannow
+pause
 GOTO MENU
 :BATTERY
 powercfg /batteryreport
+pause
 GOTO MENU
 :DISM
 DISM /online /cleanup-image /checkhealth
+DISM /online /cleanup-image /scanhealth
 pause
+DISM /online /cleanup-image /restorehealth
+pause
+GOTO MENU
+:SCSM
+sfc /scannow
+pause
+DISM /online /cleanup-image /checkhealth
 DISM /online /cleanup-image /scanhealth
 pause
 DISM /online /cleanup-image /restorehealth
