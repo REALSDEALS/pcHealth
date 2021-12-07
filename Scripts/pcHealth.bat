a0b7bbeb650161a9f5b69a454933f84d19dc23d9
REALSDEALS <realsdeals@gmail.com>
2021-12-07 21:57:02 +0100

    Add: Added Forced Admin Feature
    Add: Added the force admin feature.
    
    the program will be aware and can tell if it is being launched in administrator mode or not. When this isn't the case the prompt will ask the user to give it administrator rights.

--- Scripts/pcHealth.bat
+++ Scripts/pcHealth.bat
@@ -1,3 +1,33 @@
+@echo off
+::BatchHasAdmin
+:-------------------------------------
+REM --> Check if this file has administrator rights.
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If no rights, we don't have setted the flag for it.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
 echo off
 title pcHealth - Check your PC's Health! //\\ v0.1.2-alpha
 cd /
