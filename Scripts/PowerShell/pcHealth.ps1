10a05675da8881c345d48329cb0733cdbc760003
BigoStream <110361173+BigoStream@users.noreply.github.com>
2022-10-04 09:20:33 +0200

    Add: Added Some Features
    Add: Added some small features regarding to the 'un-loved' PowerShell script.

--- Scripts/PowerShell/pcHealth.ps1
+++ Scripts/PowerShell/pcHealth.ps1
@@ -5,7 +5,7 @@ Write-Host ""
 Write-Host "This PowerShell script is made by REALSDEALS" 
 Write-Host "Licensed under GNU-3 (You are free to use, but not to change or to remove this line.)"
 Write-Host ""
-Write-Host "You are now using PowerShell version v0.1.1-alpha"
+Write-Host "You are now using PowerShell version v0.2.1-alpha"
 Write-Host ""
 cd c:/
 
@@ -22,26 +22,27 @@ function isAdmin {
 
 function Show-Menu {
     param (
-        [string]$Title = 'pcHealth | v0.1.1-alpha'
+        [string]$Title = 'pcHealth | v0.2.1-alpha'
     )
     Clear-Host
     Write-Host ""
     Write-Host "================ $Title ================"
     Write-Host ""
-    Write-Host "1: PRESS '1' TO GATHER SYSTEM INFORMATION"
-    Write-Host "2: PRESS '2' TO CHECK WHICH CPU AND GPU ARE IN THE SYSTEM"
-    Write-Host "3: PRESS '3' RUN A SYSTEM SCAN"
-    Write-Host "4: PRESS '4' TO TRY AND REPAIR CORRUPT FILES"
-    Write-Host "5: PRESS '5' TO RUN A SYSTEM SCAN AND START REPAIRING IN ONE GO"
-    Write-Host "6: PRESS '6' TO GENERATE A BATTERY REPORT"
-    Write-Host "7: PRESS '7' TO OPEN WINDOWS UPDATE(S)"
-    Write-Host "8: PRESS '8' TO START A SHORT PING TEST"
-    Write-Host "9: PRESS '9' TO START A CONTINUES PING TEST"
-    Write-Host "10: PRESS '10' TO RE-OPEN THE BATTERY REPORT"
-    Write-Host "11: PRESS '11' TO RE-OPEN THE CBS.log (DISM LOG)"
-    Write-Host "12: PRESS '12' TO GET YOUR NINITE (INCLUDES EDGE, CHROME, VLC AND 7ZIP)"
-    Write-Host "13: PRESS '13' TO RESTART OR SHUTDOWN YOUR PC/LAPTOP"
-    Write-Host "14: PRESS 'Q' TO CLOSE THIS POWERSHELL SCRIPT"
+    Write-Host "1: ENTER '1' TO GATHER SYSTEM INFORMATION"
+    Write-Host "2: ENTER '2' TO CHECK WHICH CPU AND GPU ARE IN THE SYSTEM"
+    Write-Host "3: ENTER '3' RUN A SYSTEM SCAN"
+    Write-Host "4: ENTER '4' TO TRY AND REPAIR CORRUPT FILES"
+    Write-Host "5: ENTER '5' TO RUN A SYSTEM SCAN AND START REPAIRING IN ONE GO"
+    Write-Host "6: ENTER '6' TO GENERATE A BATTERY REPORT"
+    Write-Host "7: ENTER '7' TO OPEN WINDOWS UPDATE(S)"
+    Write-Host "8: ENTER '8' TO START A SHORT PING TEST"
+    Write-Host "9: ENTER '9' TO START A CONTINUES PING TEST"
+    Write-Host "10: ENTER '10' TO RE-OPEN THE BATTERY REPORT"
+    Write-Host "11: ENTER '11' TO RE-OPEN THE CBS.log (DISM LOG)"
+    Write-Host "12: ENTER '12' TO GET YOUR NINITE (INCLUDES EDGE, CHROME, VLC AND 7ZIP)"
+    Write-Host "13: ENTER '13' TO RESTART OR SHUTDOWN YOUR LAPTOP/PC"
+    Write-Host "14: ENTER '14' TO SHUTDOWN YOUR LAPTOP/PC"
+    Write-Host "15: ENTER 'Q' TO CLOSE THIS POWERSHELL SCRIPT"
     Write-Host ""
 }
 do {
@@ -50,7 +51,8 @@ do {
     switch ($selection) {
         '1' {
             Write-Host ""
-            systeminfo
+            Write-Host "Computer information will be generated..."
+            Get-ComputerInfo
             Write-Host ""
             pause 
         } '2' {
@@ -105,7 +107,15 @@ do {
             start explorer.exe "https://ninite.com/7zip-chrome-edge-vlc/ninite.exe"
             pause
         } '13' {
-            'Sorry, this option has no function yet.'
+            Write-Host ""
+            Write-Host "Your system will be restarted."
+            Restart-Computer
+            Write-Host ""
+            pause
+        } '14' {
+            Write-Host ""
+            Write-Host "pc-Health.ps will be terminated."
+            Write-Host ""
             pause
         }
     }
